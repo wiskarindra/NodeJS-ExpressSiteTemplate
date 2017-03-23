cb0f9af4daa316386f064984c11da7809c9a7975
Rindra Wiska <wiska.rindra@gmail.com>
2017-03-23 14:11:05 +0700

    database

--- routes/message.js
+++ routes/message.js
@@ -3,6 +3,46 @@
  * POST new-message.
  */
 
+ const axios = require('axios')
+
 exports.message = function(req, res){
-  res.render('index', { title: req.body });
+  const message = req.body
+  //console.log(message);
+  console.log(message.message.text);
+  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+
+  if (!message || message.message.text.toLowerCase().indexOf('/start') <0) {
+    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+    return res.end()
+  }
+  var target = message.message.text.toLowerCase().split(" ")[1];
+
+  connection.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected...')
+    var from = message.message.from
+    var post  = {user_id: from.id, user_name: from.first_name + " " + from.last_name, target: target, created_at: new Date()};
+    connection.query('INSERT INTO telegram_user VALUES ?', post, function(err, result) {
+      if (err) throw err
+      console.log('masuk')
+    })
+  })
+
+  // If we've gotten this far, it means that we have received a message containing the word "marco".
+  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
+  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
+  axios.post('https://api.telegram.org/bot345596833:AAGDBsKognVh4J4ALq23Sn6bmX-Dt1EfA2A/sendMessage', {
+    chat_id: message.message.chat.id,
+    text: 'Bismillah, this bot is under construction'
+  })
+    .then(response => {
+      // We get here if the message was successfully posted
+      console.log('Message posted')
+      res.end('ok')
+    })
+    .catch(err => {
+      // ...and here if it was not
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
 };
--- server.js
+++ server.js
@@ -12,9 +12,8 @@ var express = require('express')
 
 var app = express();
 var bodyParser = require('body-parser');
-const axios = require('axios')
-
-
+var mysql = require('mysql');
+var connection  = require('express-myconnection');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -30,45 +29,27 @@ app.configure(function(){
   app.use(bodyParser.urlencoded({
     extended: true
   })); // for parsing application/x-www-form-urlencoded
+  /*****************mysql connection*******************/
+  app.use(
+      connection(mysql,{
+          host: 'localhost',
+          user: 'root',
+          password : '',
+          port : 51526,
+          database:'tilawah_bot'
+      },'request')
+  );
+  /*****************************************/
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.all('/', routes.index);
+app.get('/', routes.index);
 //app.get('/users', user.list);
 //This is the route the API will call
-app.post('/new-message', function(req, res) {
-	const message = req.body
-  console.log(message);
-  console.log(message.message.text);
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-  if (!message || message.message.text.toLowerCase().indexOf('/start') <0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
-  }
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios.post('https://api.telegram.org/bot345596833:AAGDBsKognVh4J4ALq23Sn6bmX-Dt1EfA2A/sendMessage', {
-    chat_id: message.message.chat.id,
-    text: 'Bismillah, this bot is under construction'
-  })
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
-
-});
+app.post('/new-message', message.message);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
