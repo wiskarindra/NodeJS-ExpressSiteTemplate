d988c06e912b2d5b2c8faaeb43823d641ff018b2
Rindra Wiska <wiska.rindra@gmail.com>
2017-03-23 15:45:48 +0700

    update query

--- routes/message.js
+++ routes/message.js
@@ -32,25 +32,23 @@ exports.message = function(req, res){
     var post  = {user_id: from.id, user_name: from.first_name + " " + from.last_name, target: target};
     connection.query('INSERT INTO telegram_user SET ?', post, function(err, result) {
       if (err) throw err
-      console.log('masuk')
+      // If we've gotten this far, it means that we have received a message containing the word "marco".
+      // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
+      // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
+      axios.post('https://api.telegram.org/bot345596833:AAGDBsKognVh4J4ALq23Sn6bmX-Dt1EfA2A/sendMessage', {
+        chat_id: message.message.chat.id,
+        text: 'Bismillah, terima kasih ' + from.first_name + " " + from.last_name
+      })
+        .then(response => {
+          // We get here if the message was successfully posted
+          console.log('Message posted')
+          res.end('ok')
+        })
+        .catch(err => {
+          // ...and here if it was not
+          console.log('Error :', err)
+          res.end('Error :' + err)
+        })
+      };
     })
   })
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios.post('https://api.telegram.org/bot345596833:AAGDBsKognVh4J4ALq23Sn6bmX-Dt1EfA2A/sendMessage', {
-    chat_id: message.message.chat.id,
-    text: 'Bismillah, this bot is under construction'
-  })
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
-};
